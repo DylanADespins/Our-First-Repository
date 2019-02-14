49c5919c2240b34d2b5d5f3fa42d652464e85db7
Dylan Despins <02dylan.despins@gulllakecs.org>
2019-02-14 09:33:47 -0500

    Add Functions and Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -1,3 +1,27 @@
 # 4.13.3: Greeting
 # Dylan Despins
 # 2.6.19
+
+'''
+name = input("What is your name: ")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print("Nice to meet you!")
+
+greeting()
+'''
+
+
+# 4.13.4: Functions and Variables
+# Dylan Despins
+# 2.14.19
+
+x = 42
+
+def print_something():
+    x = 8
+    print(x)
+
+print_something()
+print(x)
