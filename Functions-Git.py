2d3456ca97ba3d528f02d571bf5f942d63635752
Dylan Despins <02dylan.despins@gulllakecs.org>
2019-02-27 10:03:43 -0500

    Add FName and Age

--- Functions-Git.py
+++ Functions-Git.py
@@ -45,3 +45,12 @@ def print_number(x):
 
 print_number(12)
 print_number('\n' + 'Hello World')
+
+# 4.14.4: Name and Age
+# Dylan Despins
+# 2/18/19
+
+def name_and_age(name, age):
+    print('\n' 'Hi, my name is ', name, 'and I am ',str(age), ' years old. ')
+
+name_and_age('Dylan' , 15)
